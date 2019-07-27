ce666e5bdfd801cb7ad0567a7933205331828568
Israa El-Sakka <IsraaElsakka@users.noreply.github.com>
2019-07-27 17:10:37 +0200

    Update GetVCVersion.ps1

--- GetVCVersion.ps1
+++ GetVCVersion.ps1
@@ -1,7 +1,7 @@
 function GetVCVersion {
 
-    $Server = $Global:DefaultServers
-    $Server | Select @{Name="vCenter";expression{Name}},Version,Build,@{Name="vCenterOS";expression={$Server.ExtenstionData.Content.About.OsType}} | ft
+    $vc = $Global:DefaultServers
+    $vc | Select @{Name="vCenter";expression{Name}},Version,Build,@{Name="vCenterOS";expression={$Server.ExtenstionData.Content.About.OsType}} | ft
     
 
 }
