7322a64adac7673ad9916ba184f8b35399e2de3d
Israa El-Sakka <IsraaElsakka@users.noreply.github.com>
2019-07-27 17:15:51 +0200

    Update ListClusterInVC.ps1
    List Cluster in vCenter

--- ListClusterInVC.ps1
+++ ListClusterInVC.ps1
@@ -1,3 +1,7 @@
+Connect-VIServer -Server vCenterServer -User UserName -Password Password
+$vc = $Global:DefaultServer
+$Clusters = Get-Cluster
+$hosts = Get-VMHost
 function ListClusterInVC{
 Write-Output "Clusters under vCenter $vc"
 foreach ($cls in $Clusters){
