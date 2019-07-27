73cc2266824753fe2172a0c1e6c73ab7ec17e9a6
Israa El-Sakka <IsraaElsakka@users.noreply.github.com>
2019-07-27 17:22:58 +0200

    Get vCenter name, version, build and OS.

--- GetVCVersion.ps1
+++ GetVCVersion.ps1
@@ -1,4 +1,5 @@
 function GetVCVersion {
     $vc = $Global:DefaultServers
     $vc | Select @{Name="vCenter";expression{Name}},Version,Build,@{Name="vCenterOS";expression={$Server.ExtenstionData.Content.About.OsType}} | ft
+
 }
