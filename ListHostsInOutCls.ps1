2759aa1de77d5b6adedbd7c486e5e4783f7184b8
Israa El-Sakka <IsraaElsakka@users.noreply.github.com>
2019-07-27 17:19:09 +0200

    Add files via upload

---
+++ ListHostsInOutCls.ps1
@@ -0,0 +1,26 @@
+Connect-VIServer -Server vCenterServer -User UserName -Password Password
+$vc = $Global:DefaultServer
+$Clusters = Get-Cluster
+$hosts = Get-VMHost
+
+## To List Hosts in Cluster ##
+
+function ListHostsInCluster {
+foreach ($cls in $Clusters){
+      Write-Output "Hosts under Cluster $cls"
+       $cls  | Get-VMHost | ft
+}
+}
+
+## To List Hosts outside Cluster ##
+function listHostOutCluster{
+#Get-VMHost | Select Name,Parent | Sort-Object Parent | ft
+Write-Output "Hosts Outside Cluster:"
+foreach ($h in $hosts){
+If (Get-VMHost | ?{ $_.Parent.Name -eq "host" }){
+Write-Output "$h"
+}
+else {
+Write-Output "All Hosts are in Cluster"}
+}
+}
